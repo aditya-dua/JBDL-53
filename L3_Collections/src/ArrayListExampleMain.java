dff145cb1be5a1abfa75cd1091549a4bf15a09b1
Aditya Dua <adityaduatechm@gmail.com>
2023-06-03 21:23:09 +0530

    L3 Collection - AL
    L3 Collection - AL

---
+++ L3_Collections/src/ArrayListExampleMain.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListExampleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<Integer> arList = new ArrayList<>();
+		
+		
+		arList.add(1);
+		arList.add(2);
+		arList.add(3);
+		arList.add(4);
+		
+		arList.remove(2);
+		for(int i=0;i<arList.size();i++) {
+			System.out.println(arList.get(i));
+		}
+		
+		Integer iT=new Integer(4);
+		
+		arList.remove(iT);
+		
+		for(int i=0;i<arList.size();i++) {
+			System.out.println(arList.get(i));
+		}
+		
+		for (Integer string : arList) {
+			System.out.println(string);
+		}
+		
+		System.out.println("The Size of array list id:"+arList.size());
+		
+		System.out.println(arList);
+		
+		List<String> strList = new ArrayList<>();
+		
+		strList.add("Aditya");
+		strList.add("John");
+		strList.add("David");
+		strList.add("JAck");
+		
+		
+		System.out.println(strList);
+
+		for (String string : strList) {
+			System.out.println(string);
+		}
+		
+		Iterator iterator = strList.iterator();
+		
+		while (iterator.hasNext()) {
+			String str = (String) iterator.next();
+			if(str.equals("JAck")) {
+				iterator.remove();
+			}
+			System.out.println(str);
+		}
+			
+	}
+}
