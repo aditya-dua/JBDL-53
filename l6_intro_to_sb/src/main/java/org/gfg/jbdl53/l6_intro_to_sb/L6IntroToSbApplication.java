dde6d83b1f5bbb0593cab2345b3644ea32932328
Aditya Dua <adityaduatechm@gmail.com>
2023-06-11 21:21:44 +0530

    Springboot Project
    Springboot Project

---
+++ l6_intro_to_sb/src/main/java/org/gfg/jbdl53/l6_intro_to_sb/L6IntroToSbApplication.java
@@ -0,0 +1,14 @@
+package org.gfg.jbdl53.l6_intro_to_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L6IntroToSbApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Spring boot Hello World");
+		SpringApplication.run(L6IntroToSbApplication.class, args);
+	}
+
+}
---
+++ l6_intro_to_sb/src/test/java/org/gfg/jbdl53/l6_intro_to_sb/L6IntroToSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gfg.jbdl53.l6_intro_to_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L6IntroToSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
